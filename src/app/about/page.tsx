a4dd2b66c8a5192ab002901dbecf1c71644df260
sakshamrai101 <77190532+sakshamrai101@users.noreply.github.com>
2025-02-28 20:56:12 +0530

    Heading Component Added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import { RChart } from "../components/Radar";
 
 export default function About () {
     return (
-        <div>
+        <div className="bg-[#0d1b2a]">
         <h1>About Me</h1>
         <p>I am a very motivated software engineer with an entrprenenurail spirit. It helps me own every product I work on - end to end.
            My personal mantra for success is based on precise technical acumen, strong communication and effective leadership. 
