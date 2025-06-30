ea15fb3de970aee4e45ea2f1350de8f710fd8b79
sakshamrai101 <77190532+sakshamrai101@users.noreply.github.com>
2025-06-30 00:07:47 -0700

    Landing Page Title Update

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -27,8 +27,8 @@ export default function LandingPage() {
                         </div>
 
                         <p className="text-base sm:text-lg md:text-xl font-bold text-white animate-pulse">
-                            Enginnering @CreateBase, GABRIEL AI
-                            CS UCSD'24
+                            Engineering @CreateBase, GABRIEL AI
+                            CS UCSD&apos;24
                         </p>
                         <Link href="/contact">
                             <button className="mt-3 px-3 py-2 sm:px-8 sm:py-2 md:px-5 md:py-3 text-sm sm:text-base bg-red-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-500 transition-all duration-300">
